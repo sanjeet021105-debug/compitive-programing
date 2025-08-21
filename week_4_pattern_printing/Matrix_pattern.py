c77d38e8ff84c04e2a84582b10119be1b9d0b62f
sanjeet021105-debug <sanjeet021105@gmail.com>
2025-08-21 10:07:54 +0530

    pattern printing

---
+++ week_4_pattern_printing/Matrix_pattern.py
@@ -0,0 +1,7 @@
+n= int(input("Enter N:"))
+
+
+for i in range(0,n):
+    for j in range(0,n):
+        print("*",end=" ")
+    print()
---
+++ week_4_pattern_printing/Simple_pattern.py
@@ -0,0 +1,7 @@
+n= int(input("Enter N:"))
+m= int(input("Enter :"))
+
+for i in range(0,n):
+    for j in range(0,m):
+        print("*",end=" ")
+    print()
