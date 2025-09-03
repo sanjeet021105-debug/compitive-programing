36239d23e9266d1af63e86da2bbde7bd7fe98462
sanjeet021105-debug <sanjeet021105@gmail.com>
2025-09-03 08:17:57 +0530

    list question

---
+++ list/list.py
@@ -0,0 +1,21 @@
+l =[1,2,"sanjeet",'true',5]
+
+print(l[0])
+print(l[1])
+print(l[2])
+print(l[3])
+print(l[4])
+
+print("\n",len(l))
+
+l[3]="gaurav"
+print(l)
+
+number=[10,20,30,40,50]
+sum=0
+
+for i in number:
+    sum=sum+i
+print("\n",sum)
+
+    
